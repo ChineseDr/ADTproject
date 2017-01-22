ab2de1f332b39a41a1d69d10550bac37f7d237c0
wangxingyong <wangxingyong@pset.suntec.net>
2017-01-22 11:40:27 +0800

    add jni

---
+++ jni/obj/local/armeabi/objs/jniDemo/jniDemo.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi/objs/jniDemo/jniDemo.o: jni/jniDemo.cpp \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/jni.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/api-level.h
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/jni.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/adt-bundle-windows-x86_64-20140702/android-ndk-r10e/platforms/android-17/arch-arm/usr/include/android/api-level.h:
